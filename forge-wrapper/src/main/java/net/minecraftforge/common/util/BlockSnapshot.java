e12a1bd4c996743085578eb10729f7081b86dc4a
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-09-17 20:17:06 -0400

    corrected forge-wrapper methods to exactly match forge methods

--- forge-wrapper/src/main/java/net/minecraftforge/common/util/BlockSnapshot.java
+++ forge-wrapper/src/main/java/net/minecraftforge/common/util/BlockSnapshot.java
@@ -3,12 +3,13 @@ package net.minecraftforge.common.util;
 import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 
 public class BlockSnapshot {
-    public static BlockSnapshot create(ResourceKey<?> key, Level level, BlockPos pos){
+    public static BlockSnapshot create(ResourceKey<?> key, LevelAccessor level, BlockPos pos){
         return new BlockSnapshot();
     }
 
-    public void restore(boolean a, boolean b){
+    public void restore(boolean force, boolean notifyNeighbors){
     }
 }
--- forge-wrapper/src/main/java/net/minecraftforge/event/ForgeEventFactory.java
+++ forge-wrapper/src/main/java/net/minecraftforge/event/ForgeEventFactory.java
@@ -2,23 +2,25 @@ package net.minecraftforge.event;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
 import net.minecraftforge.common.util.BlockSnapshot;
 
 import java.util.Random;
 
 public class ForgeEventFactory {
-    public static boolean onBlockPlace(Player player, BlockSnapshot snapshot, Direction direction){
+    public static boolean onBlockPlace(Entity player, BlockSnapshot snapshot, Direction direction){
         return false;
     }
 
-    public static boolean saplingGrowTree(Level level, Random random, BlockPos pos){
+    public static boolean saplingGrowTree(LevelAccessor level, Random random, BlockPos pos){
         return false;
     }
 
-    public static boolean getMobGriefingEvent(Level level, Mob mob){
+    public static boolean getMobGriefingEvent(Level level, Entity mob){
         return false;
     }
 }
