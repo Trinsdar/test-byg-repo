2d18d4132ba7fe88e87732070b7f7efbdfa98c61
Trinsdar <30245301+Trinsdar@users.noreply.github.com>
2021-09-19 16:04:55 -0400

    trying to figure out fabric noise biome placement

--- common/src/main/java/corgiaoc/byg/common/world/dimension/end/BYGEndBiomeSource.java
+++ common/src/main/java/corgiaoc/byg/common/world/dimension/end/BYGEndBiomeSource.java
@@ -10,8 +10,12 @@ import corgiaoc.byg.config.json.biomedata.BiomeData;
 import corgiaoc.byg.config.json.biomedata.BiomeDataHolders;
 import corgiaoc.byg.config.json.biomedata.WeightedBiomeData;
 import corgiaoc.byg.mixin.access.WeightedListAccess;
+import net.fabricmc.fabric.impl.biome.InternalBiomeData;
+import net.fabricmc.fabric.impl.biome.SimpleLayerRandomnessSource;
+import net.fabricmc.fabric.impl.biome.WeightedBiomePicker;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.RegistryLookupCodec;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.ai.behavior.WeightedList;
 import net.minecraft.world.level.biome.Biome;
@@ -19,12 +23,16 @@ import net.minecraft.world.level.biome.BiomeSource;
 import net.minecraft.world.level.biome.Biomes;
 import net.minecraft.world.level.biome.TheEndBiomeSource;
 import net.minecraft.world.level.levelgen.WorldgenRandom;
+import net.minecraft.world.level.levelgen.synth.ImprovedNoise;
 import net.minecraft.world.level.levelgen.synth.SimplexNoise;
+import net.minecraft.world.level.newbiome.context.Context;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -36,6 +44,7 @@ public class BYGEndBiomeSource extends BiomeSource {
     private final DatapackLayer smallIslandLayer;
     private final Registry<Biome> biomeRegistry;
     private final SimplexNoise generator;
+    Context randomnessSource;
 
     @SuppressWarnings("unchecked")
     public BYGEndBiomeSource(Registry<Biome> registry, long seed) {
@@ -119,6 +128,7 @@ public class BYGEndBiomeSource extends BiomeSource {
         this.mainIslandLayer = SimpleLayerProvider.stackLayers(this.biomeRegistry, seed, BYG.worldConfig().endBiomeSize, endBiomes, endHills, biomeToEdge);
         this.smallIslandLayer = SimpleLayerProvider.stackLayers(this.biomeRegistry, seed, BYG.worldConfig().voidBiomeSize, voidBiomes, voidHillsMap, voidBiomeToEdge);
         this.generator = new SimplexNoise(sharedseedrandom);
+        randomnessSource = new SimpleRandomSource(seed);
     }
 
     @Override
@@ -131,8 +141,8 @@ public class BYGEndBiomeSource extends BiomeSource {
         return new BYGEndBiomeSource(biomeRegistry, seed);
     }
 
-    @Override
-    public Biome getNoiseBiome(int x, int y, int z) {
+    //@Override
+    public Biome getNoiseBiome2(int x, int y, int z) {
         int xBitOffset = x >> 2;
         int zBitOffset = z >> 2;
         if ((long) xBitOffset * (long) xBitOffset + (long) zBitOffset * (long) zBitOffset <= 4096L) {
@@ -147,7 +157,7 @@ public class BYGEndBiomeSource extends BiomeSource {
         }
     }
 
-    public Biome getNoiseBiome2(int x, int y, int z) {
+    public Biome getNoiseBiome(int x, int y, int z) {
         int xBitOffset = x >> 2;
         int zBitOffset = z >> 2;
         if ((long)xBitOffset * (long)xBitOffset + (long)zBitOffset * (long)zBitOffset <= 4096L) {
@@ -155,12 +165,60 @@ public class BYGEndBiomeSource extends BiomeSource {
         } else {
             float sampledNoise = TheEndBiomeSource.getHeightValue(this.generator, xBitOffset * 2 + 1, zBitOffset * 2 + 1);
             if (sampledNoise > 40.0F) {
-                return biomeRegistry.getOrThrow(Biomes.END_HIGHLANDS);
+                return getWeightedEndBiome(x, y, z, biomeRegistry.getOrThrow(Biomes.END_HIGHLANDS));
             } else if (sampledNoise >= 0.0F) {
                 return biomeRegistry.getOrThrow(Biomes.END_MIDLANDS);
             } else {
                 return sampledNoise < -20.0F ? biomeRegistry.getOrThrow(Biomes.SMALL_END_ISLANDS) : biomeRegistry.getOrThrow(Biomes.END_BARRENS);
             }
+        }
+    }
+
+    private Biome getWeightedEndBiome(int biomeX, int biomeY, int biomeZ, Biome vanillaBiome) {
+
+        // Since all vanilla biomes are added to the registry, this will never fail.
+        ResourceKey<Biome> vanillaKey = biomeRegistry.getResourceKey(vanillaBiome).get();
+        ResourceKey<Biome> replacementKey;
+
+        // The x and z of the biome are divided by 64 to ensure custom biomes are large enough; going larger than this]
+        // seems to make custom biomes too hard to find.
+        if (vanillaKey == Biomes.END_MIDLANDS || vanillaKey == Biomes.END_BARRENS) {
+            // Since the highlands picker is statically populated by InternalBiomeData, picker will never be null.
+            WeightedBiomePicker highlandsPicker = InternalBiomeData.getEndBiomesMap().get(Biomes.END_HIGHLANDS);
+            ResourceKey<Biome> highlandsKey = highlandsPicker.pickFromNoise(randomnessSource, biomeX/64.0, 0, biomeZ/64.0);
+
+            if (vanillaKey == Biomes.END_MIDLANDS) {
+                WeightedBiomePicker midlandsPicker = InternalBiomeData.getEndMidlandsMap().get(highlandsKey);
+                replacementKey = (midlandsPicker == null) ? vanillaKey : midlandsPicker.pickFromNoise(randomnessSource, biomeX/64.0, 0, biomeZ/64.0);
+            } else {
+                WeightedBiomePicker barrensPicker = InternalBiomeData.getEndBarrensMap().get(highlandsKey);
+                replacementKey = (barrensPicker == null) ? vanillaKey : barrensPicker.pickFromNoise(randomnessSource, biomeX/64.0, 0, biomeZ/64.0);
+            }
+        } else {
+            // Since the main island and small islands pickers are statically populated by InternalBiomeData, picker will never be null.
+            WeightedBiomePicker picker = InternalBiomeData.getEndBiomesMap().get(vanillaKey);
+            replacementKey = picker.pickFromNoise(randomnessSource, biomeX*64.0, 0, biomeZ*64.0);
+        }
+
+        return biomeRegistry.get(replacementKey);
+    }
+
+    public static class SimpleRandomSource implements Context{
+        private final ImprovedNoise sampler;
+        private final Random random;
+        public SimpleRandomSource(long seed){
+            this.random = new Random(seed);
+            this.sampler = new ImprovedNoise(random);
+        }
+
+        @Override
+        public int nextRandom(int i) {
+            return random.nextInt(i);
+        }
+
+        @Override
+        public ImprovedNoise getBiomeNoise() {
+            return sampler;
         }
     }
 }
